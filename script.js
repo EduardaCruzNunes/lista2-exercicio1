5c758634544a0a7d45c2674992ef8730b097fa67
unknown <Eduarda Nunes>
2022-10-28 23:18:57 -0300

    exercicio-1

---
+++ script.js
@@ -0,0 +1,13 @@
+var nota1 = 9;
+var nota2 = 7;
+var nota3 = 8;
+var media = 0;
+
+media = (nota1 + nota2 + nota3) / 3;
+if(media >= 6)
+{
+    console.log('O valor da média é igual a ', media, 'Aprovado!');
+}else
+{
+    console.log('O valor da média é igual a ', media, 'Reprovado!');
+}
